b300a1cfa36a966513198db33f53b7d6144a7f17
adam <adam@e8884077-4c9d-4443-b904-cf31936a3ca2>
2013-08-12 22:37:52 +0000

    Working 1.0

--- hyp-mobile-detect.php
+++ hyp-mobile-detect.php
@@ -8,10 +8,32 @@ Version: 1.0
 Author URI: http://hyperspatial.com
 */
 
+/*
+//https://github.com/serbanghita/Mobile-Detect#usage
+//Usage
+$hmd = new HMD();
+if( $hmd->device()->isAndroidOS() ) echo 'Android Device';
+else echo 'Not Android';
+echo '<br /><br />';
+if( $hmd->device()->isIOS() ) echo 'IOS Device';
+else echo 'Not IOS Device';
+echo '<br /><br />';
+echo  'deviceType = ' . $hmd->device_type();
+*/
+
 //File Paths
 define('HMD_PLUGIN',WP_PLUGIN_URL . '/' . basename(dirname(__FILE__)) . '/');
 define('HMD_PLUGIN_SERVERPATH',dirname(__FILE__) . '/');
 
 require HMD_PLUGIN_SERVERPATH . 'Mobile_Detect.php';
 
+class HMD{
+	private $_device,$_device_type;
+	public function __construct(){
+		$this->_device = new Mobile_Detect();
+		$this->_device_type = ($this->_device->isMobile() ? ($this->_device->isTablet() ? 'tablet' : 'phone') : 'computer');
+	}
+	public function device(){ return $this->_device; }
+	public function device_type(){ return $this->_device_type; }
+}
 ?>
